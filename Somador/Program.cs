b6c4242fe70f80d2cac13c7f9d778b5bc4e22261
Gustavo Ribeiro <pokegustavo@gmail.com>
2020-11-10 17:46:26 -0300

    teste V1

---
+++ Somador/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Somador {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Quantos números você quer somar? " );
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Então somaremos {N} números");
+
+            int Soma = 0;
+            for (int L = 1 ; L <= N; L++) {
+                Console.Write($"Número {L}:" );
+                int R = int.Parse(Console.ReadLine());
+                Soma += R;
+            }
+            Console.WriteLine($"A soma dos números é: {Soma}");
+
+        }
+    }
+}
